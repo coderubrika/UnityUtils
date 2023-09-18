5ddc3b8c6b31ce603015ba9197a91be8ec4c8de9
Anton Samartsev <coderubrika@gmail.com>
2023-09-18 20:47:34 +0300

    initial

---
+++ GeneralUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UniRx;
+
+namespace Suburb.Utils
+{
+    public static class GeneralUtils
+    {
+        public static IObservable<T> StartWithDefault<T> (T defaultValue = default)
+        {
+            return Observable.Start(() => defaultValue);
+        }
+
+        public static string GetUID()
+        {
+            return DateTimeOffset.Now
+                .ToUnixTimeMilliseconds()
+                .ToString();
+        }
+    }
+}
