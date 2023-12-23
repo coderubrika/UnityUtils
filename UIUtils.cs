6f84ac1534b069ffaac266d588e47813fd9a799a
Anton Samartsev <coderubrika@gmail.com>
2023-12-23 03:46:30 +0300

    improve

--- UIUtils.cs
+++ UIUtils.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using DG.Tweening;
+using Suburb.Utils.Serialization;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
@@ -39,6 +41,14 @@ namespace Suburb.Utils
         public static Color GetNewAlpha(Color source, float alpha)
         {
             return new Color(source.r, source.g, source.b, alpha);
+        }
+
+        public static Tween FadeCanvas(CanvasGroup canvasGroup, ValueAnimationData<float> config)
+        {
+            canvasGroup.alpha = config.Start;
+            return canvasGroup.DOFade(config.End, config.AnimationSettings.Duration)
+                .SetEase(config.AnimationSettings.Easing)
+                .OnKill(() => canvasGroup.alpha = config.End);
         }
     }
 }
